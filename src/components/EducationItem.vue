09b0c3d295adbde194a2be2bd81e6886fadf71ac
chestercolita <chester@nabepero.co.jp>
2021-09-15 00:44:41 +0800

    Update education

--- src/components/EducationItem.vue
+++ src/components/EducationItem.vue
@@ -5,8 +5,12 @@
       <h1 class="postcard__title blue">{{ school.name }}</h1>
       <h5 class="postcard__subtitle">{{ school.degree }}</h5>
       <div class="postcard__bar"></div>
-      <div class="postcard__preview-txt">{{ school.year }}</div>
-      <div class="postcard__preview-txt">{{ school.remarks }}</div>
+      <div class="postcard__preview-txt">
+        <span>Year Attended: </span>{{ school.year }}
+      </div>
+      <div class="postcard__preview-txt">
+        <span>Thesis: </span>{{ school.remarks }}
+      </div>
     </div>
   </div>
 </template>
@@ -22,4 +26,10 @@ export default {
 
 <style scoped lang="scss">
 @import "../assets/scss/postcard.scss";
+
+span {
+  --bs-text-opacity: 0.9;
+  color: rgba(var(--bs-info-rgb),var(--bs-bg-opacity))!important;
+  font-weight: 600;
+}
 </style>
