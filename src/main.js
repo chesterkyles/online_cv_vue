46ef9d18ea0d83a8e794c684a1a13624d31cf6ce
chestercolita <chester@nabepero.co.jp>
2021-09-15 19:29:45 +0800

    Fix asset and lazy loading

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
-import VueParticles from 'vue-particles'
-import VTooltip from 'v-tooltip'
-
 import App from './App'
 import router from './router'
+import VueParticles from 'vue-particles'
+import VTooltip from 'v-tooltip'
+import VueLazyload from 'vue-lazyload'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'v-tooltip/dist/v-tooltip.css'
 
 Vue.use(VueParticles)
 Vue.use(VTooltip)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: `${process.env.BASE_URL}error.png`,
+  loading: `${process.env.BASE_URL}loading.gif`,
+  attempt: 1
+})
 
 Vue.config.productionTip = false
 
